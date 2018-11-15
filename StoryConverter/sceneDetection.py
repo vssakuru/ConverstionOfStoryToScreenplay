f2b287c35254b3659a843de4ce28df0e27490e00
Unaiza Faiz <unaizafaiz@gmail.com>
2018-11-15 10:38:22 -0600

    Data structure for ER

--- StoryConverter/sceneDetection.py
+++ StoryConverter/sceneDetection.py
@@ -12,20 +12,40 @@ def tokenizaParagraphs(fileName):
 
 def startsWith(token):
     #token = dp.removePunctuations(token)
-    print(token)
+    #print(token)
     with open("./SceneTransitionLexicon.txt") as lexicon:
         for line in lexicon:
             #print(line)
             line = line.replace('\n', '').replace('\r', '')
             if token.startswith(line):
-                print(line)
+                #print(line)
                 return True
     return False
 
-# startsWith("One day they made the porridge for their breakfast, and poured it into their porridge-pots, and then went out in the wood for a walk while the porridge for their breakfast was cooling. And while they were out walking, a little Old Woman came to the house in the wood and peeped inside.")
 
 paragraphTokens = tokenizaParagraphs("./Data/Train/adventuresOfTomThumb.txt")
 
+
+##detecting entities for each paragraph
+listER = []
+for i in range(0,len(paragraphTokens)):
+    #....detect character ... location and time as string
+    character = ['a','b','c']
+    location = 'palace'
+    time = 'day'
+    dict = {'paragraphText':paragraphTokens[i], 'character':character, 'location':location, 'time':time}
+    listER.append(dict)
+
+for i in range(0,len(listER)):
+    print(listER[i]['paragraphText'])
+    print(listER[i]['character'])
+    print(listER[i]['location'])
+    print(listER[i]['time'])
+    print("\n\n")
+
+
+
+##Using lexicon to detect scenes
 scenes = []
 scene = ""
 for i in range(0,len(paragraphTokens)):
